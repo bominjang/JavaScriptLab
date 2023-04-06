76aee5a2f1bb9e5ac17e6fb612a7e8bfdf5003bf
bominjang <qhdud9443@naver.com>
2023-04-07 00:28:12 +0900

    [+] array - push

--- momentum/app.js
+++ momentum/app.js
@@ -1,13 +1,9 @@
-const mon = "mon";
-const tue = "tue";
-const wed = "wed";
-const thu = "thu";
-const fri = "fri";
-const sat = "sat";
-const sun = "sun";
+const daysOfWeek = ["mon","tue","wed","thu","fri","sat"];
 
-const daysOfWeek = [mon,tue,wed,thu,fri,sat,sun];
+// Get Item from Array
+console.log(daysOfWeek);
 
-const nonsense = [1,2,"hello",false,null,true,undefined,"bomin"]
+// Add one more day to the array
+daysOfWeek.push("sun")
 
-console.log(daysOfWeek, nonsense)
+console.log(daysOfWeek)
