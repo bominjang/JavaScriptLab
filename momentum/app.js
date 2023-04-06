8b1d4eb81272851950fd99461f6b9fc0034b667f
bominjang <qhdud9443@naver.com>
2023-04-07 00:15:10 +0900

    [+] list

---
+++ momentum/app.js
@@ -0,0 +1,13 @@
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfWeek = [mon,tue,wed,thu,fri,sat,sun];
+
+const nonsense = [1,2,"hello",false,null,true,undefined,"bomin"]
+
+console.log(daysOfWeek, nonsense)
