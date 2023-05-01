fcd70f30e49221e5d40a656d96a73684ee34ab81
bominjang <qhdud9443@naver.com>
2023-05-02 00:44:21 +0900

    [+] Events

--- momentum/app.js
+++ momentum/app.js
@@ -1,3 +1,9 @@
-const title = document.querySelector(".hello h1");
+const title = document.querySelector("div.hello:first-child h1");
 
-console.log(title);
+function handleTitleClick(){
+    title.style.color = "blue";
+    console.log("title was clicked!");
+}
+
+// user가 title을 클릭할 경우, Javascript가 대신 handleTitleClick을 실행
+title.addEventListener("click", handleTitleClick);
