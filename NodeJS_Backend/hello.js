08c3f5457c86893d7557a22dd562cb6c68ce828a
장보민 <bmjang.vcsl@gmail.com>
2023-05-03 14:50:48 +0900

    [+] Refactor Router using Map

--- NodeJS_Backend/hello.js
+++ NodeJS_Backend/hello.js
@@ -4,13 +4,12 @@ http.createServer((req, res) => {
     const path = url.parse(req.url, true).pathname; //url 모듈을 사용해 패스명 할당
     res.setHeader("Content-Type", "text/html");
 
-    if (path === "/user") {
-        user(req, res);
-    } else if (path === "/feed") {
-        feed(req, res);
+    if (path in urlMap) {
+        urlMap[path](req, res);
     } else {
         notFound(req, res);
     }
+        
 })
     .listen("3000", () => console.log("라우터를 만들어보자!"));
 
@@ -32,3 +31,9 @@ const notFound = (req, res) => {
     res.statusCode = 404;
     res.end("404 page not found");
 }
+
+const urlMap = {
+    "/": (req, res) => res.end("HOME"),
+    "/user": user,
+    "/feed": feed,
+};
