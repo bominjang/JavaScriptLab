228ddddaac9a414a77323cf1ee9129334091f160
장보민 <bmjang.vcsl@gmail.com>
2023-05-03 17:02:08 +0900

    [+] Made requesting post function

--- NodeJS_Backend/chapter3/express-server/hello-express.js
+++ NodeJS_Backend/chapter3/express-server/hello-express.js
@@ -1,33 +1,44 @@
-//기존에는 http 인스턴스의 createServer() 함수 내에서 라우팅과 결과 처리를 했지만, 익스프레스를 사용해
-//좀 더 쉽게 더 좋은 라우팅 기능 사용 가능
-const url = require("url");
 const express = require("express");//express 모듈 불러오기
 const app = express(); //express를 초기화 후 app에 할당
-const port = 3000;
+let posts = []; // 1. 게시글 리스트로 사용할 posts에 빈 리스트 할당. 글 삭제 시, 삭제된 목록으로 재할당하기 때문에 let으로 지정
 
-app.listen(port, () => {
-    console.log("익스프레스로 라우터 리펙토링하기");
+// 2. req.body를 사용하려면 JSON 미들웨어를 사용해야 함.
+// 사용하지 않으면 undefined로 반환
+app.use(express.json());
+
+//POST 요청 시 컨텐트 타입이 application/x-www-form-urlencoded인 경우 파싱
+//application/x-www-form-urlencoded 타입 : body에 키=값&키2=값2같은 키=값 조합 형태의 데이터
+// app.use() : 미들웨어 사용 시
+app.use(express.urlencoded({ extended: true }));//3. json 미들웨어와 함께 사용
+
+app.get("/", (req, res) => { // 4. /로 요청이 오면 실행
+    res.json(posts); //5. 게시글 리스트를 JSON 형식으로 보여줌
+    //list, json 데이터를 처리할 수 있는 json()함수 사용
 });
 
-// GET 메서드의 라우팅 설정
-app.get("/", (_, res) => res.end("HOME"));
-app.get("/user", user);
-app.get("/feed", feed);
+app.post("/posts", (req, res) => {
+    const { title, name, text } = req.body; //7. HTTP 요청의 body 데이터를 변수에 할당
 
-function user(req, res) {
-    const user = url.parse(req.url, true).query;
+    // 8. 게시글 리스트에 새로운 게시글 정보 추가
+    posts.push({ id: posts.length + 1, title, name, text, createdDt: Date() });
+    res.json({ title, name, text});
+});
 
-    //결과값으로 유저명과 나이 제공
-    res.json(`[user] name : ${user.name}, age: ${user.age}`);
-}
+app.delete("/posts/:id", (req, res) => {
+    const id = req.params.id; //9.
+    //10.삭제 로직
+    //id 이외의 글들만 뽑아서 filteredPosts에 다시 할당
+    //+id : 문자열인 id를 integer형으로 변경한다는 뜻. == parseInt
+    const filteredPosts = posts.filter((post) => post.id !== +id); 
+    const isLengthChanged = posts.length !== filteredPosts.length; //11.삭제 확인
+    posts = filteredPosts;
+    if (isLengthChanged) { //12. posts의 데이터 개수가 변경되었으면 삭제 성공
+        res.json("OK");
+        return;
+    }
+    res.json("NOT CHANGED"); //13. 변경되지 않음
+});
 
-function feed(_, res) {// _ : 사용하지 않는 변수이지만, 인터페이스 구조상 넣을 수 밖에 없을 때 사용.
-    //json()를 사용하면, 응답 타입을 JSON으로 보여주기도 하고, charset=utf-8을 자동으로 설정해주므로 한글을 간단히 처리 가능
-    res.json(`<ul>
-        <li>picture1</li>
-        <li>picture2</li>
-        <li>picture3</li>
-        </ul>
-        `);
-}
-
+app.listen(3000, () => {
+    console.log("Welcome posts START!");
+});
