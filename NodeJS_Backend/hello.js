800a3d5865a4efe95d7ab513c7088dfe223fc54a
장보민 <bmjang.vcsl@gmail.com>
2023-05-03 14:19:26 +0900

    [+]Made Router

--- NodeJS_Backend/hello.js
+++ NodeJS_Backend/hello.js
@@ -1,7 +1,21 @@
 const http = require("http");
-const server = http.createServer((req, res) => {
-    res.setHeader("Content-Type", "text/html"); //응답의 헤더 설정
-    res.end("OK"); //OK를 응답하고 종료
-});
+const url = require("url"); //url 모듈을 로드
+http.createServer((req, res) => {
+    const path = url.parse(req.url, true).pathname; //패스명 할당
+    res.setHeader("Content-Type", "text/html");
 
-server.listen("3000", () => console.log("OK 서버 시작!")); //접속 대기
+    if (path === "/user") {
+        res.end("[user] name : andy, age: 30");
+    } else if (path === "/feed") {
+        res.end(`<ul>
+        <li>picture1</li>
+        <li>picture2</li>
+        <li>picture3</li>
+        </ul>
+        `);
+    } else {
+        res.statusCode = 404;
+        res.end("404 page not found");
+    }
+})
+    .listen("3000", () => console.log("라우터를 만들어보자!"));
