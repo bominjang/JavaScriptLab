684ecc7417360222388143118703fb66adbc4244
장보민 <bmjang.vcsl@gmail.com>
2023-07-15 17:15:55 +0900

    [+]#5.2 useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,9 +1,17 @@
+import { useState, useEffect } from "react";
 import styles from "./App.module.css";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("Call the API....");
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
