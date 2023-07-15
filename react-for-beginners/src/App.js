48c36686d9c2ca4492312e749e77a4cb50b9279b
장보민 <bmjang.vcsl@gmail.com>
2023-07-15 17:35:50 +0900

    [+]#5.3 useEffect Full

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -3,13 +3,33 @@ import styles from "./App.module.css";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
   console.log("I run all the time");
+
   useEffect(() => {
-    console.log("Call the API....");
+    console.log("I run only once....");
   }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes ....");
+  }, [keyword]); //keyword가 변경될때만 console.log 수행하도록
+  useEffect(() => {
+    console.log("I run when 'counter' changes ....");
+  }, [counter]); //counter 변경될때만 console.log 수행하도록
+  useEffect(() => {
+    console.log("I run when counter or keyword change");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here...."
+      />
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
